90ec6711ae93d4622d40fe9d14535ac04f7757a5
Jude Hansen <>
2021-04-28 00:51:28 -0700

    publish workflow figured out; site generated and published

---
+++ public/component---src-pages-404-js-081f6c816e3df73eb29d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"page not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-081f6c816e3df73eb29d.js.map
